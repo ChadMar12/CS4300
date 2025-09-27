2b2236f6f8b59d01afe7911ec5eb2487625a3ee1
Chad Marshall <chadmarshall50@gmail.com>
2025-09-27 03:32:59 +0000

    Save point

---
+++ homework2/django_intro/bookings/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Movie, Booking, Seat  
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class SeatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Seat
+        fields = '__all__'
--- homework2/django_intro/bookings/views.py
+++ homework2/django_intro/bookings/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
-from .models import Movie, Seat, Booking
+from bookings.models import Movie, Seat, Booking
+from bookings.serializers import MovieSerializer, SeatSerializer, BookingSerializer
 
 
 # Create your views here.
@@ -10,33 +11,30 @@ from .models import Movie, Seat, Booking
 #Empty for the time being
 class MovieViewSet(viewsets.ReadOnlyModelViewSet):
     
-    def create(self, request) :
-        pass 
+    queryset = Movie.objets.all()
+    serializers_class = ModelSerializer
 
-    def read(self, request):
-        pass
-
-    def update(self, request):
+    def create(self, request):
         pass
     
-    def destroy(self, request):
+    def update(self, request, pk=None):
         pass
     
+    def read(self, request, pk=None):
+        pass
+    
+    def destroy(self, request, pk=None):
+        pass
+
 #SeatViewSet: For seat availability and booking.
 class SeatViewSet():
     
-    def seat_availability():
-        pass
-    
-    def book_seat():
-        pass
- 
+    queryset = Movie.objets.all()
+    serializers_class = SeatSerializer
+     
 
 #BookingViewSet: For users to book seats and view their booking history.
 class BookingViewSet():
 
-    def book_seat():
-        pass
-    
-    def view_seat():
-        pass
+    queryset = Movie.objets.all()
+    serializers_class = BookingSerializer
